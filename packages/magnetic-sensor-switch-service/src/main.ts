94b4f393903eb9df10c3213805d819b4ce65e45a
Szymon Standarski <s.standarski@gmail.com>
2022-03-25 23:27:12 +0100

    add johnny-five library and simple test

--- packages/magnetic-sensor-switch-service/src/main.ts
+++ packages/magnetic-sensor-switch-service/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import { Board, Led } from "johnny-five";
+// const five = require("johnny-five");
+
+console.log("Hello World!");
+
+const board = new Board();
+
+board.on("ready", function () {
+  // Create an Led on pin 27
+  const led = new Led(27);
+
+  // Strobe the pin on/off, defaults to 100ms phases
+  led.strobe(1000);
+});
